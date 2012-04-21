a7b34563a24742a8b777548cd37f09f3be4a91dc
Georges Racinet on ishtar.racinet.fr <georges@racinet.fr>
2012-04-21 12:39:03 +0200

    make_slaves mock implementation

--- anybox/buildbot/openerp/__init__.py
+++ anybox/buildbot/openerp/__init__.py
@@ -1,1 +1,12 @@
 """Provides utilities to be imported from the master.cfg"""
+from buildbot.buildslave import BuildSlave
+
+def make_slaves(conf_path):
+    """Create the slave objects from the file at conf_path.
+
+    For now, this is a hardcoded mock-up. The conf_path is ignored.
+    Ideally, if conf_path is allowed to be relative, it'll be interpreted from
+    the buildmaster base directory.
+    """
+
+    return [BuildSlave('local', 'local', properties=dict(pg_version='8.4'))]
