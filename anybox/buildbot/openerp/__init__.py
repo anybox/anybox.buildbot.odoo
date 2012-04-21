1e59b25d29df3394d8936ca7e70328e65363f8a9
Georges Racinet on ishtar.racinet.fr <georges@racinet.fr>
2012-04-21 13:52:53 +0200

    Minimal hardcoding of buildfactory and spawning of builders

--- anybox/buildbot/openerp/__init__.py
+++ anybox/buildbot/openerp/__init__.py
@@ -1,12 +1,46 @@
 """Provides utilities to be imported from the master.cfg"""
 from buildbot.buildslave import BuildSlave
+from buildbot.config import BuilderConfig
+
+BUILDMASTER_DIR = '' # monkey patched with actual value from master.cfg
 
 def make_slaves(conf_path):
     """Create the slave objects from the file at conf_path.
 
-    For now, this is a hardcoded mock-up. The conf_path is ignored.
-    Ideally, if conf_path is allowed to be relative, it'll be interpreted from
-    the buildmaster base directory.
+    ``conf_path`` is interpreted relative from BUILDMASTER_DIR. It can of
+    course be an absolute path.
+    For now, this function is a hardcoded mock-up. The conf_path is ignored.
     """
 
     return [BuildSlave('local', 'local', properties=dict(pg_version='8.4'))]
+
+
+def make_builders(master_config=None, build_factories=None):
+    """Create builders from build factories using the whole buildmaster config.
+
+    build_factories is a dict names -> build_factories
+
+    The idea is notably to sort slaves according to capabilities (for specific
+    requirements, such as ability to build a tricky python package) and
+    environmental parameters (postgresql version etc.)
+
+    To demonstrate, for now, we iterate on pg_version
+    """
+
+    # they are kwarg for the sake of expliciteness
+    assert master_config is not None
+    assert build_factories is not None
+
+    slaves = master_config['slaves']
+
+    slaves_by_pg = {} # pg version -> list of slave names
+    for slave in slaves:
+        pg = slave.properties['pg_version']
+        slaves_by_pg.setdefault(pg, []).append(slave.slavename)
+
+    return [BuilderConfig(name='%s-postgresql-%s' % (factory_name, pg_version),
+                          factory=factory, slavenames=slavenames)
+            for pg_version, slavenames in slaves_by_pg.items()
+            for factory_name, factory in build_factories.items()]
+
+
