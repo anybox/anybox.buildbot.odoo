d4da5f799710f6d916e1a85f88f6b7c6d7c544c6
gr <>
2012-05-07 16:41:29 +0200

    Version matcher mostly ready

--- anybox/buildbot/openerp/version.py
+++ anybox/buildbot/openerp/version.py
@@ -85,14 +85,29 @@ class Version(object):
 class VersionFilter(object):
     """Represent a simple version filter.
 
-    >>> vf = VersionFilter('pg', [('>=', Version.parse('9.1')),
-    ...                           ('<', Version.parse('9.3'))])
-    >>> vf.match(Version.parse('9.2'))
-    True
-    >>> vf.match(Version.parse('8.4'))
-    False
-    >>> vf.match(Version.parse('9.3-devel'))
-    True
+    The simplest way to instantiate is to call the ``parse`` classmethod
+    (see also its docstring)::
+
+      >>> vf = VersionFilter.parse('pg >= 9.1 < 9.3')
+
+    Then we can tell if a given version matches::
+
+      >>> vf.match(Version.parse('9.2'))
+      True
+      >>> vf.match(Version.parse('8.4'))
+      False
+      >>> vf.match(Version.parse('9.3-devel'))
+      True
+
+    With more complicated criteria::
+      >>> vf = VersionFilter.parse('pg >= 9.2-devel OR == 8.4-special')
+      >>> vf.match(Version.parse('9.2'))
+      True
+      >>> vf.match(Version.parse('9.1'))
+      False
+      >>> vf.match(Version.parse('8.4-special'))
+      True
+
     """
 
     def __init__(self, capability, criteria):
@@ -104,11 +119,54 @@ class VersionFilter(object):
     def match(self, version):
         """Tell if the given version matches the criteria."""
 
-        for op, crit_version in self.criteria:
-            if (op == '>=' and not version >= crit_version or
-                op == '==' and not version == crit_version or
-                op == '<=' and not version <= crit_version or
-                op == '<' and not version < crit_version or
-                op == '>' and not version > crit_version):
-                return False
+        return self.boolean_match(version, self.criteria)
+
+    def __repr__(self):
+        return 'VersionFilter(%r, %r)' % (self.cap, self.criteria)
+
+    @classmethod
+    def boolean_parse(cls, reqline):
+        ors = reqline.split('OR', 1)
+        if len(ors) == 2:
+            return ('OR', cls.boolean_parse(ors[0].strip()),
+                    cls.boolean_parse(ors[1].strip()))
+
+        split = reqline.split(' ', 2)
+        vreq = (split[0], Version.parse(split[1]))
+        if len(split) == 2:
+            return vreq
+
+        return 'AND', vreq, cls.boolean_parse(split[2])
+
+    def boolean_match(self, version, criteria):
+        op = criteria[0]
+
+        # binary ops
+        if op.upper() == 'OR':
+            return (self.boolean_match(version, criteria[1]) or
+                    self.boolean_match(version, criteria[2]))
+        elif op.upper() == 'AND':
+            return (self.boolean_match(version, criteria[1]) and
+                    self.boolean_match(version, criteria[2]))
+
+        # unary ops
+        crit_version = criteria[1]
+        if (op == '>=' and not version >= crit_version or
+                    op == '==' and not version == crit_version or
+                    op == '<=' and not version <= crit_version or
+                    op == '<' and not version < crit_version or
+                    op == '>' and not version > crit_version):
+                    return False
         return True
+
+    @classmethod
+    def parse(cls, as_string):
+        """Parse the filter from a requirement line.
+
+        ANDs are implicit between operands, OR are not and take precedence.
+        >>> VersionFilter.parse('postgresql <= 9.2 > 8.4 OR == 8.4-patched')
+        VersionFilter('postgresql', ('OR', ('AND', ('<=', Version(9, 2)), ('>', Version(8, 4))), ('==', Version(8, 4, suffix='patched'))))
+        """
+        cap, req = as_string.split(' ', 1)
+        return cls(cap, cls.boolean_parse(req))
+
