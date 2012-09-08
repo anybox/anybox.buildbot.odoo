a61d4b7799150517eedb199cd85f3c6e8db5f92a
Georges Racinet <gracinet@anybox.fr>
2012-09-08 13:59:07 +0200

    Correct version string for 0.8.7 branch

--- setup.py
+++ setup.py
@@ -26,7 +26,9 @@ setup(
                    'anybox/buildbot/openerp/tests/data/one_slave.cfg',
                  ])],
     namespace_packages=['anybox', 'anybox.buildbot'],
-    install_requires=['buildbot == latest'], # future 0.8.7
+    install_requires=['buildbot == latest', # future 0.8.7
+                      'bzr',
+                      ]
     tests_require=['nose'],
     test_suite='nose.collector',
     classifiers=[
