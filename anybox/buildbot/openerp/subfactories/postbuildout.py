anybox/anybox.buildbot.odoo

commit eba21e49673239cf929b6a858fe23e97bade95c4

Made cache directories absolute
Referring to them from the inside of build/ could not work uniformly due
to potential dereferencing of symlinks.

anybox/buildbot/openerp/subfactories/postbuildout.py
@@ -376,7 +376,8 @@ def functional(configurator, options, buildout_slave_path,
         steps.append(ShellCommand(
             command=['bin/buildout',
                      '-c', buildout_slave_path,
-                     'buildout:eggs-directory=../../buildout-caches/eggs',
+                     WithProperties('buildout:eggs-directory='
+                                    '$(builddir)s/../buildout-caches/eggs'),
                      'install'] + buildout_parts,
             name="functional tools",
             description=['install', 'functional', 'buildout', 'parts'],
@@ -477,8 +478,9 @@ def static_analysis(configurator, options, buildout_slave_path, environ=()):
     steps.append(
         ShellCommand(command=['bin/buildout',
                               '-c', buildout_slave_path,
-                              'buildout:eggs-directory='
-                              '../../buildout-caches/eggs',
+                              WithProperties(
+                                  'buildout:eggs-directory='
+                                  '$(builddir)s/../buildout-caches/eggs'),
                               'install',
                               options.get('static-analysis.part',
                                           'static-analysis')

anybox/buildbot/openerp/configurator.py
@@ -209,7 +209,7 @@ class BuildoutsConfigurator(object):
         # path in that situation, that is likely to be the simplest of their
         # tunings
         command = ['python', bootstrap_options.pop('script', 'bootstrap.py'),
-                   find_links_opt, eggs_cache,
+                   find_links_opt, WithProperties(eggs_cache),
                    '-c', buildout_slave_path]
         command.extend('--%s=%s' % (k, v)
                        for k, v in bootstrap_options.items())
@@ -280,12 +280,13 @@ class BuildoutsConfigurator(object):
             factory.build_requires = set(VersionFilter.parse(r)
                                          for r in requires.split(os.linesep))
 
-        factory.addStep(ShellCommand(command=['bzr', 'init-repo', '../..'],
+        factory.addStep(ShellCommand(command=['bzr', 'init-repo', '..'],
                                      name="bzr repo",
                                      description="init bzr repo",
                                      flunkOnFailure=False,
                                      warnOnFailure=False,
                                      hideStepIf=True,
+                                     workdir='.',
                                      ))
         map(factory.addStep, buildout_dl_steps)
 
@@ -310,12 +311,14 @@ class BuildoutsConfigurator(object):
         ))
 
         buildout_part = options.get('buildout-part', DEFAULT_BUILDOUT_PART)
-        cache = '../../buildout-caches'
+        cache = '%(builddir)s/../buildout-caches'
         eggs_cache = cache + '/eggs'
         openerp_cache = cache + '/openerp'
         factory.addStep(ShellCommand(command=['mkdir', '-p',
-                                              eggs_cache, openerp_cache],
+                                              WithProperties(eggs_cache),
+                                              WithProperties(openerp_cache)],
                                      name="cachedirs",
+                                     workdir='.',
                                      description="prepare cache dirs"))
 
         map(factory.addStep,
@@ -324,8 +327,9 @@ class BuildoutsConfigurator(object):
         factory.addStep(ShellCommand(command=[
             'bin/buildout',
             '-c', buildout_slave_path,
-            'buildout:eggs-directory=' + eggs_cache,
-            'buildout:openerp-downloads-directory=' + openerp_cache,
+            WithProperties('buildout:eggs-directory=' + eggs_cache),
+            WithProperties('buildout:openerp-downloads-directory='
+                           + openerp_cache),
             buildout_part + ':with_devtools=true',
             buildout_part + ':vcs-clear-locks=true',
             buildout_part + ':vcs-clear-retry=true',

anybox/buildbot/openerp/subfactories/postdownload.py
@@ -54,7 +54,7 @@ def packaging(configurator, options,
                               description="cleaning",
                               workdir='.'))
 
-    cache = '../../buildout-caches'  # lame duplication
+    cache = '%{builddir)s/../buildout-caches'  # lame duplication
     eggs_cache = cache + '/eggs'
     openerp_cache = cache + '/openerp'
     archive_name_interp = options['packaging.prefix'] + '-%(buildout-tag)s'
@@ -75,9 +75,10 @@ def packaging(configurator, options,
 
     steps.append(
         ShellCommand(command=['bin/buildout', '-c', buildout_slave_path,
-                              'buildout:eggs-directory=' + eggs_cache,
-                              'buildout:openerp-downloads-'
-                              'directory=' + openerp_cache,
+                              WithProperties('buildout:eggs-directory='
+                                             + eggs_cache),
+                              WithProperties('buildout:openerp-downloads-'
+                                             'directory=' + openerp_cache),
                               'install'] + parts,
                      description=["buildout", "install"],
                      workdir='./src',
@@ -85,8 +86,9 @@ def packaging(configurator, options,
                      ))
 
     extract_cmd = ['bin/buildout', '-o', '-c', buildout_slave_path,
-                   'buildout:eggs-directory=' + eggs_cache,
-                   'buildout:openerp-downloads-directory=' + openerp_cache,
+                   WithProperties('buildout:eggs-directory=' + eggs_cache),
+                   WithProperties('buildout:openerp-downloads-directory='
+                                  + openerp_cache),
                    ]
     extract_cmd.extend(WithProperties(
         ('%s:extract-downloads-to=../dist/' % part) + archive_name_interp)

commit 3ac0ac616dde193e8a71be5c1fe8c9d5c35f27ed

Two prop syntax error introduced in [eba21e4]

anybox/buildbot/openerp/subfactories/postbuildout.py
@@ -377,7 +377,7 @@ def functional(configurator, options, buildout_slave_path,
             command=['bin/buildout',
                      '-c', buildout_slave_path,
                      WithProperties('buildout:eggs-directory='
-                                    '$(builddir)s/../buildout-caches/eggs'),
+                                    '%(builddir)s/../buildout-caches/eggs'),
                      'install'] + buildout_parts,
             name="functional tools",
             description=['install', 'functional', 'buildout', 'parts'],
@@ -480,7 +480,7 @@ def static_analysis(configurator, options, buildout_slave_path, environ=()):
                               '-c', buildout_slave_path,
                               WithProperties(
                                   'buildout:eggs-directory='
-                                  '$(builddir)s/../buildout-caches/eggs'),
+                                  '%(builddir)s/../buildout-caches/eggs'),
                               'install',
                               options.get('static-analysis.part',
                                           'static-analysis')