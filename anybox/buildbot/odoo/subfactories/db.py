8cafdc9f0dffb2bdf6c841ef24b89e7a96d25949
fjouatte <fjouatte@anybox.fr>
2018-08-16 10:15:23 +0200

    fix use 'testing_db' property

--- anybox/buildbot/odoo/subfactories/db.py
+++ anybox/buildbot/odoo/subfactories/db.py
@@ -32,11 +32,11 @@ def simple_create(configurator, options, environ=()):
         command=[
             'psql', 'postgres',
             WithProperties(
-                "-d \"%%(testing_db)s\" -c "
-                "INSERT INTO ir_mail_server "
-                "(smtp_host, smtp_port, name, smtp_encryption) VALUES "
-                "('disabled.test', 25, "
-                "'Disabled (adresses in .test are unroutable)', 'none'"
+                '-d "%%(testing_db)s" -c '
+                'INSERT INTO ir_mail_server '
+                '(smtp_host, smtp_port, name, smtp_encryption) VALUES '
+                '("disabled.test", 25, '
+                '"Disabled (adresses in .test are unroutable)", "none"'
             ),
         ],
         name='create_disabled_outgoing_mail_server',
